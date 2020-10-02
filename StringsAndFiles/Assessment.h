32fce67f293c2a1128bcd8f69a209269dae0adba
Brandyn <brandyn_xavier@hotmail.com>
2020-10-01 23:00:32 -0400

    Added setup for header files

--- StringsAndFiles/Assessment.h
+++ StringsAndFiles/Assessment.h
@@ -1,2 +1,23 @@
-#pragma once
-//will hold data for an assessment handler
+#ifndef DATA_ASSESSMENT_H_
+#define DATA_ASSESSMENT_H_
+#include <string>
+namespace data {
+	class Prompts {
+		std::string m_topic;
+		std::string m_prompt;
+		int difficulty;
+
+
+		std::string getTopic();
+		std::string getPrompt();
+		int getDifficulty();
+	public:
+		bool loadPrompts(const std::string filename);
+		bool loadObjects(Prompts& f_prompt);
+		void display();
+		void memDel();
+	};
+}
+
+
+#endif
--- StringsAndFiles/FileHandler.h
+++ StringsAndFiles/FileHandler.h
@@ -1,2 +1,12 @@
-#pragma once
-//handles files
+#ifndef DATA_FILEHANDLER_H_
+#define DATA_FILEHANDLER_H_
+#include <string>
+namespace data {
+	bool openFile(const std::string filename);
+	void closeFile();
+	int recordCount();
+	bool read(std::string data);
+	bool read(std::string subject);
+	bool read(int& difficulty);
+}
+#endif
